f4eb1a7d089667c342d321f0c01c9280a1d324d9
abhinavshinow <55484189+alphabeta2206@users.noreply.github.com>
2020-10-16 19:31:03 +0530

    initial commit

---
+++ magicui/android/app/src/main/kotlin/com/example/magicui/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.magicui
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ magicui/lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:magicui/homepage.dart';
+void main() => runApp(MAGICUI());
+class MAGICUI extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "Scan QR code",
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+        appBarTheme: AppBarTheme(
+          color: Colors.transparent,
+          elevation: 0.0,
+        ),
+        scaffoldBackgroundColor: Colors.transparent,
+      ),
+      home: Homepage(),
+    );
+  }
+}

f5d4459ece1313cc78010dbb3c229c802c3a3b6e
abhinavshinow <55484189+alphabeta2206@users.noreply.github.com>
2020-10-16 23:48:25 +0530

    appbarchanges

--- magicui/lib/homepage.dart
+++ magicui/lib/homepage.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+
 class Homepage extends StatefulWidget {
   Homepage({Key key}) : super(key: key);
 
@@ -9,8 +10,55 @@ class Homepage extends StatefulWidget {
 class _HomepageState extends State<Homepage> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-       child: child,
+    return Scaffold(
+      appBar: PreferredSize(
+        preferredSize: Size.fromHeight(90.0),
+        child: Container(
+          decoration: BoxDecoration(
+            gradient: LinearGradient(
+              begin: Alignment.topRight,
+              end: Alignment.bottomLeft,
+              stops: [0.1, 0.4, 0.7, 0.9],
+              colors: [
+                Colors.blue[600],
+                Colors.blue[500],
+                Colors.blue[400],
+                Colors.blue[300],
+              ],
+            ),
+          ),
+          child: Column(
+            children: [
+              Container(
+                margin: EdgeInsets.fromLTRB(4, 0, 4, 10),
+                child: Row(
+                  children: [
+                    IconButton(
+                      icon: Icon(Icons.view_headline,
+                          color: Colors.black54, size: 30.0),
+                      onPressed: () {
+                        /* Side TAB BUTTON */
+                      },
+                    ),
+                    Spacer(),
+                    IconButton(
+                        icon: Icon(Icons.add_shopping_cart,
+                            color: Colors.black54, size: 25.0),
+                        onPressed: () {
+                          /* ADD TO CART BUTTON */
+                        })
+                  ],
+                ),
+              ),
+              Container(
+                decoration: BoxDecoration(
+                  
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
     );
   }
 }

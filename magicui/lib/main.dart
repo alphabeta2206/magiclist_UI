699ae0e65704ad06185c6526f5f09381fdfc75a2
abhinavshinow <55484189+alphabeta2206@users.noreply.github.com>
2020-11-03 12:06:57 +0530

    qr dep changed

--- magicui/lib/main.dart
+++ magicui/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:magicui/morse.dart';
 import 'package:magicui/qrgenarate.dart';
 import 'package:magicui/xsepwindow.dart';
 import 'package:magicui/qrscanpage.dart';
@@ -19,7 +20,7 @@ class MAGICUI extends StatelessWidget {
         ),
         scaffoldBackgroundColor: Colors.white,
       ),
-      home: ScanPage(),
+      home: Qrscan(),
     );
   }
 }
